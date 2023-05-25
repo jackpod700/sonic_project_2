5f8e39df401a7ed06e816893296b9af6cabd723a
DESKTOP-0TEKJIF\Yrina <hj3416song@naver.com>
2023-05-25 11:43:54 +0900

    맵선택 노래 추가

--- 메뉴/프로젝트/map/map.js
+++ 메뉴/프로젝트/map/map.js
@@ -1,6 +1,21 @@
+var bgVol;
+function controlSound() {
+  $("#sound-bg").prop("volume", bgVol);
+}
 function start(num){
     location.href = "../item_ex/explain"+num+".html";
 }
+function controlMusic() {
+  window.focus();
+  $("#sound-bg").get(0).play();
+
+  setTimeout(function () {
+    controlMusic();
+  }, 1);
+}
+
+
+
 $("#sonicmap").click(function(){
     start(1)
 })
@@ -9,4 +24,11 @@ $("#shadowmap").click(function(){
 })
 $("#eggmanmap").click(function(){
     start(3)
+})
+
+$(document).ready(function () {
+
+    bgVol = localStorage.getItem("bgVol");
+    controlSound();
+    controlMusic();
 })
